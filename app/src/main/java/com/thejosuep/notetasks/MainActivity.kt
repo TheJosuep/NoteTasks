7e04525214fd653acead203d3150617bda13cf8b
DJosuep <josuep.garciar@gmail.com>
2023-07-15 16:11:15 -0600

    Import lifecycle and add navigation
    - Import lifecycle dependency.
    - Add navigation graphs to Navigation.

--- app/src/main/java/com/thejosuep/notetasks/MainActivity.kt
+++ app/src/main/java/com/thejosuep/notetasks/MainActivity.kt
@@ -10,34 +10,28 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.thejosuep.notetasks.navigation.Navigation
 import com.thejosuep.notetasks.ui.theme.NoteTasksTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
+
+            val navController: NavHostController = rememberNavController()
+
             NoteTasksTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Navigation(navController)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NoteTasksTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/thejosuep/notetasks/navigation/Navigation.kt
+++ app/src/main/java/com/thejosuep/notetasks/navigation/Navigation.kt
@@ -1,6 +1,112 @@
 package com.thejosuep.notetasks.navigation
 
-fun Navigation(){
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+
+@Composable
+fun Navigation(
+    navController: NavHostController
+){
+
+    NavHost(
+        navController = navController,
+        startDestination = Screens.MainScreen.route
+    ){
+
+        composable(route = Screens.MainScreen.route){
+            
+        }
+
+        // Notes graph
+        navigation(
+            startDestination = Screens.NotesScreen.route,
+            route = Graphs.NotesGraph.route
+        ){
+            composable(route = Screens.NotesScreen.route){
+
+            }
+        }
+
+        // ToDo graph
+        navigation(
+            startDestination = Screens.ToDoScreen.route,
+            route = Graphs.ToDoGraph.route
+        ){
+            composable(route = Screens.ToDoScreen.route){
+
+            }
+        }
+
+        // DoNot graph
+        navigation(
+            startDestination = Screens.DoNotScreen.route,
+            route = Graphs.DoNotGraph.route
+        ){
+            composable(route = Screens.DoNotScreen.route){
+
+            }
+        }
+
+        // Last visited graph
+        navigation(
+            startDestination = Screens.HistoryScreen.route,
+            route = Graphs.HistoryGraph.route
+        ){
+            composable(route = Screens.HistoryScreen.route){
+
+            }
+        }
+
+        // Search graph
+        navigation(
+            startDestination = Screens.SearchScreen.route,
+            route = Graphs.SearchGraph.route
+        ){
+            composable(route = Screens.SearchScreen.route){
+
+            }
+        }
+
+        // About the app screen
+        composable(route = Screens.AboutScreen.route){
+
+        }
+    }
+}
+
+sealed class Graphs(val route: String){
+    object MainGraph: Graphs("main_graph")
+    object NotesGraph: Graphs("notes_graph")
+    object ToDoGraph: Graphs("to_do_graph")
+    object DoNotGraph: Graphs("do_not_graph")
+    object HistoryGraph: Graphs("history_graph")
+    object SearchGraph: Graphs("search_graph")
+}
+
+sealed class Screens(val route: String){
+    object MainScreen: Screens("main_screen")
+    object NotesScreen: Screens("notes_screen")
+    object ToDoScreen: Screens("to_do_screen")
+    object DoNotScreen: Screens("do_not_screen")
+    object HistoryScreen: Screens("history_screen")
+    object SearchScreen: Screens("search_screen")
+    object AboutScreen: Screens("about_screen")
+}
 
 
+@Composable
+inline fun <reified T: ViewModel> NavBackStackEntry.sharedViewModel(navController: NavHostController): T {
+    val navGraphRoute = destination.parent?.route ?: return viewModel()
+    val parentEntry = remember(this){
+        navController.getBackStackEntry(navGraphRoute)
+    }
+
+    return viewModel(parentEntry)
 }
